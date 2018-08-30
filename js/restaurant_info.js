819e5d0cb2f89c2971d4678c22f8d14125f8a310
Robert Leu <robertleu@gmail.com>
2018-08-29 19:18:51 -0700

    Created Service Worker for Caching

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,3 +1,21 @@
+/**
+ * Check for serviceWorker support
+ * If supported, register the worker after window load. (https://developers.google.com/web/fundamentals/primers/service-workers/registration)
+ * If not supported, display error.
+ */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker
+            // Service worker scope is entire app
+            // because it is placed in root folder
+            .register('../sw_restaurant.js')
+            // Worker Registered
+            .then(reg => console.log('Service Worker Registered Successfully'))
+            // Catching the error (err)
+            .catch(err => console.log('Service Worker encountered and error: ' + `${err}`));
+    });
+}
+
 let restaurant;
 var newMap;
 var mapBox_KEY = mapBox.MY_KEY;
---
+++ sw_restaurant.js
@@ -0,0 +1,84 @@
+/**
+ * Service Worker Lifecycle for reference
+ * Register : registered in main.js and restaurant_info.js
+ * Install
+ * Activate
+ */
+
+// Use variable to easily manage caches if changed are made
+const cacheVersion = 'version1';
+// Array of items to Cache
+const cacheItems = [
+    'sw_restaurant.js',
+    'index.html',
+    'restaurant.html',
+    '/css/media-queries.css',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'config.js',
+    'https://necolas.github.io/normalize.css/8.0.0/normalize.css',
+    'https://unpkg.com/leaflet@1.3.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.3.3/dist/leaflet.js'
+];
+
+/**
+ * Install event for the Service Worker
+ */
+self.addEventListener('install', event => {
+    // Wait until promise is completed
+    event.waitUntil(
+        caches
+            .open(cacheVersion)
+            // Open cache and add all items from array to cache
+            .then(cache => {
+                cache.addAll(cacheItems);
+            })
+            // skipWaiting for items can go into effect immediately
+            .then(() => self.skipWaiting)
+    );
+});
+
+/**
+ * Activate event for the Service Worker
+ * * Check for cache matches and delete older cache for future
+ * * cache changes and storage cleanup
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        // Grab caches keys and map through any and all caches then to check if caches match
+        caches.keys().then(cacheVersions => {
+            return Promise.all(
+                cacheVersions.map(cache => {
+                    // If cache does not equal to cacheVersion variable
+                    if (cache !== cacheVersion) {
+                        // Delete the cache
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+/**
+ * Fetch event for Service Worker when offline or network difficulties
+ */
+self.addEventListener('fetch', event => {
+    // Check if site is working by repsonding with initial request
+    // If no request, catch the response and return the cache that matches
+    // the request.
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
